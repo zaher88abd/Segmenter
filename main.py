2e0dfdb53dc14b3fbee82466b5845abb99155b3e
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-04-16 10:02:08 -0300

    Change to  GraphicView

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 import os
-from PyQt5 import QtCore
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QGraphicsScene
 from PyQt5.uic import loadUi
 from lib.filter import *
 
@@ -14,6 +13,8 @@ class Segmeter(QDialog):
         loadUi('main.ui', self)
         self.image = None
         self.openBtn.clicked.connect(self.openFile)
+        self.scene = QGraphicsScene()
+        self.f_view.setScene(self.scene)
 
     @pyqtSlot()
     def openFile(self):
@@ -26,35 +27,44 @@ class Segmeter(QDialog):
         self.displayImage()
 
     def displayImage(self):
-        qformat = QImage.Format_Indexed8
-        if len(self.image.shape) == 3:
-            if (self.image.shape[2]) == 4:
-                qformat = QImage.Format_RGBA8888
-            else:
-                qformat = QImage.Format_RGB888
+        try:
+            self.scene.clear()
+            qformat = QImage.Format_Indexed8
+            qformatg=QImage.Format_Indexed8
+            if len(self.image.shape) == 3:
+                if (self.image.shape[2]) == 4:
+                    qformat = QImage.Format_RGBA8888
+                else:
+                    qformat = QImage.Format_RGB888
 
-        self.f_image = filter_image(self.image, 1)
-        height, width = self.image.shape[:2]
-        max_height = 471
-        max_width = 341
+            self.f_image = filter_image(self.image, 1)
+            height, width = self.image.shape[:2]
+            max_height = 512
+            max_width = 384
 
-        # only shrink if img is bigger than required
-        if max_height < height or max_width < width:
-            # get scaling factor
-            scaling_factor = max_height / float(height)
-            if max_width / float(width) < scaling_factor:
-                scaling_factor = max_width / float(width)
-            # resize image
-            self.image = cv2.resize(self.image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+            # only shrink if img is bigger than required
+            if max_height < height or max_width < width:
+                # get scaling factor
+                scaling_factor = max_height / float(height)
+                if max_width / float(width) < scaling_factor:
+                    scaling_factor = max_width / float(width)
+                # resize image
+                self.image = cv2.resize(self.image, None, fx=scaling_factor, fy=scaling_factor,
+                                        interpolation=cv2.INTER_AREA)
 
-        img = QImage(self.image, self.image.shape[1], self.image.shape[0], self.image.strides[0], qformat)
-        img = img.rgbSwapped()
+            img = QImage(self.image, self.image.shape[1], self.image.shape[0], self.image.strides[0], qformat)
+            img = img.rgbSwapped()
 
-        f__img = QImage(self.f_image, self.f_image.shape[1], self.f_image.shape[0], self.f_image.strides[0], qformat)
-        self.orgImg.setPixmap(QPixmap.fromImage(img))
-        self.orgImg.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-        self.filImg.setPixmap(QPixmap.fromImage(f__img))
-        self.filImg.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+            self.orgImg.setPixmap(QPixmap.fromImage(img))
+            self.orgImg.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+            fimg = QImage(self.f_image, self.f_image.shape[1], self.f_image.shape[0], self.f_image.strides[0], qformatg)
+            pixMap = QPixmap.fromImage(fimg)
+            self.scene.addPixmap(pixMap)
+            self.f_view.fitInView(QRectF(0, 0, self.f_image.shape[1],self.f_image.shape[0]), Qt.KeepAspectRatio)
+            self.scene.update()
+        except Exception as e:
+            print(e)
 
 
 app = QApplication(sys.argv)
