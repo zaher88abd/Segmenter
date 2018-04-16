e5333163a299953ffe188b21477f13b65d6b7c2f
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-04-16 09:13:19 -0300

    Change the layout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import cv2
 import sys
+import os
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QImage, QPixmap
@@ -10,7 +10,7 @@ from lib.filter import *
 
 class Segmeter(QDialog):
     def __init__(self):
-        super(Segmeter, self).__init__()
+        super().__init__()
         loadUi('main.ui', self)
         self.image = None
         self.openBtn.clicked.connect(self.openFile)
@@ -32,12 +32,29 @@ class Segmeter(QDialog):
                 qformat = QImage.Format_RGBA8888
             else:
                 qformat = QImage.Format_RGB888
-        self.fimage = filter()
+
+        self.f_image = filter_image(self.image, 1)
+        height, width = self.image.shape[:2]
+        max_height = 471
+        max_width = 341
+
+        # only shrink if img is bigger than required
+        if max_height < height or max_width < width:
+            # get scaling factor
+            scaling_factor = max_height / float(height)
+            if max_width / float(width) < scaling_factor:
+                scaling_factor = max_width / float(width)
+            # resize image
+            self.image = cv2.resize(self.image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
         img = QImage(self.image, self.image.shape[1], self.image.shape[0], self.image.strides[0], qformat)
         img = img.rgbSwapped()
-        f_img = QImage()
+
+        f__img = QImage(self.f_image, self.f_image.shape[1], self.f_image.shape[0], self.f_image.strides[0], qformat)
         self.orgImg.setPixmap(QPixmap.fromImage(img))
         self.orgImg.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+        self.filImg.setPixmap(QPixmap.fromImage(f__img))
+        self.filImg.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
 
 app = QApplication(sys.argv)
