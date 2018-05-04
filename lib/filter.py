b77c6f55292e78a960a9e6d3972634c396704aae
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-05-03 22:50:58 -0300

    Fix none filter

--- lib/filter.py
+++ lib/filter.py
@@ -3,14 +3,14 @@ import lib.augment_data as ad
 
 
 def filter_image(original_img, filter_num):
-    if filter_num == 0:
-        return original_img
     rgb_img = original_img  # cv2.imread(original_img, cv2.IMREAD_UNCHANGED)
 
     proper_h = 384
     proper_w = 512
     # resize to see how it works with kernels
     rgb_img = ad.resize(rgb_img, [proper_h, proper_w])
+    if filter_num == 0:
+        return rgb_img
     if rgb_img is None:
         print("Rgb image doesn't exist")
         quit()
